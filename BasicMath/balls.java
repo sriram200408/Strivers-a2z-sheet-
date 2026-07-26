7db3a5c7d90fdcf8b0660f2d5dcebdd9615c15a9
sriram200408 <f20221082@pilani.bits-pilani.ac.in>
2026-07-26 13:00:50 +0530

    math

---
+++ BasicMath/balls.java
@@ -0,0 +1,23 @@
+public class balls{
+    public static void main(String[] args) {
+        int total = 0;
+
+        
+        total = (total-9) + (total-9) + (total-9) + (total-9);
+       total = 36/3;
+               int red = total - 9;
+        int blue = total - 9;
+        int green = total - 9;
+        int yellow = total - 9;
+       
+       System.out.println(total);
+       System.out.println(red);
+       System.out.println(blue);
+       System.out.println(green);
+       System.out.println(yellow);
+
+        
+
+
+    }
+}
